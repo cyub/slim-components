8e0e29f5d289574e0ad022978a949531d71430ae
cyub <qietingfy@gmail.com>
2017-08-03 20:57:32 +0800

    fix a bug

--- src/Facades/App.php
+++ src/Facades/App.php
@@ -1,10 +1,10 @@
-<?php
+<?php 
 
 namespace Tink\Common\Facades;
 
 class App extends Facade
 {
-	protected static function getFacadeAccessor() { return self::$container['application']; }
+	protected static function getFacadeAccessor() { return self::$container; }
 
 	public static function make($key)
 	{
@@ -18,19 +18,26 @@ class App extends Facade
 
 	public static function instance()
 	{
-		return self::$app['app'];
+		return self::$app['application'];
 	}
 
 	public static function singleton($name, $value)
 	{
 		self::$app[$name] = function () use ($value) {
 			return $value;
-		}
+		};
+
+		return self::$app->get($name);
 	}
 
 	public static function basePath()
 	{
-		return realpath(dirname($_SERVER['DOCUMENT_ROOT']));
+		return self::$app->get('basePath');
+	}
+
+	public static function configPath()
+	{
+		return self::basePath() . '/' . 'config/';
 	}
 
 	public static function environment()
